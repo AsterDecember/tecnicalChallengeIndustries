c750f15f65275ec0ad14cc3a5860309e32d387c1
AsterDecember <aster.december@gmail.com>
2019-01-25 13:17:37 -0600

    token working correctly test1

--- routes/app.js
+++ routes/app.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const App = require('../models/App');
-const fs   = require('fs');
-const jwt  = require('jsonwebtoken');
-const privateKEY  = fs.readFileSync('../keys/private.key', 'utf8');
-const publicKEY  = fs.readFileSync('../keys/public.key','utf8');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+const privateKEY = fs.readFileSync(__dirname + '/private.key', 'utf8');
+const publicKEY  = fs.readFileSync(__dirname+ '/public.key', 'utf8');
 //TODO: JWT on response
+
 router.post('/register', (req, res, next) => {
-    res.json(privateKEY)
-    /*App.create(req.body)
-        .then(response => {
-            res.json(response)
-        })
-        .catch(e => res.json(e))*/
+    const {user} = req.body
+    const signOptions = {
+        expiresIn: "12h",
+        algorithm: "RS256"
+    };
+    const token = jwt.sign({
+        codeBy: 'AsterDecember'
+    }, privateKEY, signOptions);
+    res.json(token)
 })
 
-
+router.get('/verify',(req,res,next)=>{
+    const token = req.headers.authorization
+    const verifyOptions = {
+        expiresIn: "12h",
+        algorithm: "RS256"
+    };
+    let legit = jwt.verify(token, publicKEY, verifyOptions);
+    res.json(legit)
+})
 module.exports = router;
